8f8c851ee47381e570b702dd2e09f6a82a68a952
Mark Renzo Santiago <msantiago8@ucmerced.edu>
2015-10-11 10:02:59 -0700

    Create credentials.php

---
+++ credentials.php
@@ -0,0 +1,13 @@
+<!-- 
+@Author: Mark renzo Santiago
+Cal Hacks 2015
+ -->
+<?php
+
+// credential information to access local DB on Mark's computer :)
+$username = '';
+$password = '';
+$dbname = 'CalHacks2015';
+$servername = 'localhost';
+
+?>
